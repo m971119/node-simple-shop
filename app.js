b09432e24063bf4509fdec37ece61590b196d60f
Michelle Lin <m971119@gmail.com>
2022-10-21 21:45:26 +0800

    feat: add 404 page

--- app.js
+++ app.js
@@ -15,4 +15,8 @@ const shopRoutes = require('./routes/shop');
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send(`<h1>Page Not Found</h1>`);
+});
+
 app.listen(3000);
