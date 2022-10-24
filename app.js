08a856ae5797b87abf2c20ac2bf65292538d8ea7
Michelle Lin <m971119@gmail.com>
2022-10-24 18:07:59 +0800

    refactor: add rootDir to util, avoid relative path

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('./util/path');
+
 const app = express();
 
 // add this to be able to parse res.body
@@ -17,7 +19,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
