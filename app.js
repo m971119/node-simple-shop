591ac11b64ba8ca96cce8c3a86e39d286f2d545f
Michelle Lin <m971119@gmail.com>
2022-11-21 15:18:43 +0800

    feat: add product controller and model
    - data source is json file, should be modified to connect DB

--- app.js
+++ app.js
@@ -1,31 +1,24 @@
-const http = require('http');
 const path = require('path');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const rootDir = require('./util/path');
+const errorController = require('./controllers/error');
 
 const app = express();
 
 app.set('view engine', 'ejs');
-// Set where the views files are
 app.set('views', 'views');
 
-// add this to be able to parse res.body
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-// Serve Files statically
-app.use(express.static(path.join(rootDir, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
-});
+app.use(errorController.get404);
 
 app.listen(3000);
