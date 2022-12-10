e6571456eb50dd9a53226932e803badae2a52e2c
Michelle Lin <m971119@gmail.com>
2022-12-10 10:43:04 +0800

    chore: connect db via sequelize

--- util/database.js
+++ util/database.js
@@ -1,11 +1,13 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-});
-
-module.exports = pool.promise();
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        dialect: 'mysql',
+        host: process.env.DB_HOST
+    }
+);
+module.exports = sequelize;
